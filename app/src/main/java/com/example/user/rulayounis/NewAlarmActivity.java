67ae86655de8e1a2841258eafaf5c4937dcb4467
rulaYounis <rul.ayns@gmail.com>
2018-10-08 13:36:59 +0300

    datePicker

--- app/src/main/java/com/example/user/rulayounis/NewAlarmActivity.java
+++ app/src/main/java/com/example/user/rulayounis/NewAlarmActivity.java
@@ -1,28 +1,36 @@
 package com.example.user.rulayounis;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class NewAlarmActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class NewAlarmActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener,View.OnClickListener{
+    Button btSetDate,btSetAlarm,btChooseActivity;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_alarm);
-        Button btSetAlarm = (Button) findViewById(R.id.btSetAlarm);
-        btSetAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DialogFragment timePicker= new TimePickerFragment();
-                timePicker.show(getSupportFragmentManager(),"time picker");
-            }
-        });
+        btSetAlarm = (Button) findViewById(R.id.btSetAlarm);
+        btSetAlarm.setOnClickListener(this);
+        btChooseActivity = (Button) findViewById(R.id.btChooseActivity);
+        btChooseActivity.setOnClickListener(this);
+        btSetDate= (Button)findViewById(R.id.btSetDate);
+        btSetDate.setOnClickListener(this);
+
 
     }
 
@@ -31,7 +39,39 @@ public class NewAlarmActivity extends AppCompatActivity implements TimePickerDia
         TextView tvTime=(TextView) findViewById(R.id.tvTime);
         tvTime.setText(hourOfDay+":"+minute);
     }
-    Button btSetDate= (Button)findViewById(R.id.btSetDate);
 
 
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR,year);
+        c.set(Calendar.MONTH,month);
+        c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String currentDateString= DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        TextView tvDate =(TextView) findViewById(R.id.tvDate);
+        tvDate.setText(currentDateString);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == btSetDate){
+            DialogFragment datePicker= new DatePickerFragment();
+            datePicker.show(getSupportFragmentManager(),"date picker");
+
+        }
+        if(v== btSetAlarm){
+            DialogFragment timePicker= new TimePickerFragment();
+            timePicker.show(getSupportFragmentManager(),"time picker");
+        }
+        if(v== btChooseActivity){
+            Intent intent= new Intent(getApplication(),ChooseAccountActivity.class);
+            startActivity(intent);
+        }
+
+
+    }
 }
