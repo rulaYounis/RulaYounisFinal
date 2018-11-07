6ad8def2b80a9d796b77c926af8868c32a4b6657
rulaYounis <rul.ayns@gmail.com>
2018-11-07 14:00:11 +0200

    firebase database

--- app/src/main/java/com/example/user/rulayounis/FireActivity.java
+++ app/src/main/java/com/example/user/rulayounis/FireActivity.java
@@ -2,12 +2,14 @@ package com.example.user.rulayounis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,6 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class FireActivity extends AppCompatActivity {
     EditText etEmail1,etPass;
@@ -24,26 +27,67 @@ public class FireActivity extends AppCompatActivity {
     ArrayList<String> users;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire);
         final FirebaseDatabase  database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef= database.getReference().child("Name");
+        final DatabaseReference myRef= database.getReference();
         etEmail1= (EditText) findViewById(R.id.etEmail1);
         etPass = (EditText) findViewById(R.id.etPass);
         btSave=( Button) findViewById(R.id.btSave);
+/*        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email= etEmail1.getText().toString();
+                myRef.child("Users/Name").setValue(email);
+
+
+            }
+        });*/
         tvEmail= (TextView) findViewById(R.id.tvEmail);
         tvProfession= (TextView) findViewById(R.id.tvProfession);
         lvUsers = (ListView) findViewById(R.id.lvUsers);
         users= new ArrayList<String>();
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
         lvUsers.setAdapter(adapter);
-        myRef.addValueEventListener(new ValueEventListener() {
+/*        myRef.child("Users").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                tvEmail.setText(value);
+                Map<String,String> map = (Map<String, String>) dataSnapshot.getValue();
+                String name = map.get("Name");
+                String profession = map.get("Profession");
+                tvEmail.setText(name);
+                tvProfession.setText(profession);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });*/
+        myRef.child("Users").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String name = dataSnapshot.getValue(String.class);
+                users.add(name);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
