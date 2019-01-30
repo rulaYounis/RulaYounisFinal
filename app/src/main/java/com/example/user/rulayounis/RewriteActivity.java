6bdfb17824b74dd3608e47b712766543e6686716
rulaYounis <rula.yns@gmail.com>
2019-01-30 13:33:19 +0200

    changed the names of the tasks

--- app/src/main/java/com/example/user/rulayounis/RewriteActivity.java
+++ app/src/main/java/com/example/user/rulayounis/RewriteActivity.java
@@ -2,12 +2,26 @@ package com.example.user.rulayounis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class RewriteActivity extends AppCompatActivity {
+
+    ArrayList<String> arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rewrite);
+        arrayList.add("My friends and I are going to the cinema tonight ");
+        arrayList.add("We should work hard in order to get what we want");
+        arrayList.add("math task");
+        arrayList.add("questions task");
+        arrayList.add("synonyms task");
+        int position=(int)((Math.random() * (10))+1);
+
+
+
     }
 }
