0a12785b38db522d1aeb6f0918a69508eb2f8314
rulaYounis <rula.yns@gmail.com>
2019-02-04 13:38:16 +0200

    rewrite mission(next time add the button)

--- app/src/main/java/com/example/user/rulayounis/RewriteActivity.java
+++ app/src/main/java/com/example/user/rulayounis/RewriteActivity.java
@@ -2,26 +2,67 @@ package com.example.user.rulayounis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class RewriteActivity extends AppCompatActivity {
+public class RewriteActivity extends AppCompatActivity implements TextWatcher{
 
     ArrayList<String> arrayList = new ArrayList<>();
-
+    TextView tvMain;
+    TextView tvSuccess;
+    EditText etReWrite;
+    int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rewrite);
+        tvMain = (TextView) findViewById(R.id.tvMain);
+        tvSuccess = (TextView) findViewById(R.id.tvSuccess);
+        etReWrite =(EditText) findViewById(R.id.etReWrite);
         arrayList.add("My friends and I are going to the cinema tonight ");
         arrayList.add("We should work hard in order to get what we want");
         arrayList.add("math task");
         arrayList.add("questions task");
         arrayList.add("synonyms task");
-        int position=(int)((Math.random() * (10))+1);
+        arrayList.add("questions task");
+        arrayList.add("questions task");
+        arrayList.add("questions task");
+        arrayList.add("questions task");
+        arrayList.add("questions task");
+        position=(int)((Math.random() * (9))+1);
+        tvMain.setText(arrayList.get(position));
+        etReWrite.addTextChangedListener(this);
 
 
 
+
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+        if(s.equals(arrayList.get(position))){
+            tvSuccess.setText("correct");
+        }
+        else {
+            tvSuccess.setText("incorrect");
+        }
     }
 }
