cffd8639839fdba93b2571b0bda29b0206034e20
rulaYounis <rula.yns@gmail.com>
2019-02-22 08:32:59 +0200

    notification

---
+++ app/src/main/java/com/example/user/rulayounis/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.user.rulayounis;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Intent intent1 = new Intent(context, MyNewIntentService.class);
+
+        context.startService(intent1);
+    }
+}
